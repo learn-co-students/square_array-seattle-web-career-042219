2502f390a9c85a4e2b6e14d544c73b14dfbac690
Jennifer Kim <jjkim429@gmail.com>
2019-04-08 20:51:24 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,5 @@
 def square_array(array)
-  squared_array = []
-  squared_array.push(array.each do
-    array[] ** 2)
+  squared = []
+  array.each { |element| squared << element ** 2 }
+  squared
 end
